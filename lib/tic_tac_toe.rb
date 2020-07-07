a9b074c75b432433f567a52f4facf08d4d15cb9b
Daniel Gould <dmgould310@gmail.com>
2020-07-07 15:35:04 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,10 @@
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # First Row down 
+  [1,4,7], # Second row down
+  [2,5,8], # Third row down 
+  [0,4,8], # First Diagonal 
+  [2,4,6], # Second Diagonal 
+]
