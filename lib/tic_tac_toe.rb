a42d33f15578834facb5b2a724fb66ff758b7635
Daniel Gould <dmgould310@gmail.com>
2020-07-07 15:48:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,7 +19,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index
+def input_to_index(user_input)
   user_input = gets.chomp
   user_input.to_s -= 1 
   end 
