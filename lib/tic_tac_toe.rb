08d7d55355e09fda7d3ef634110818ace7f93b2a
Daniel Gould <dmgould310@gmail.com>
2020-07-07 16:14:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,8 @@ def turn(board)
   puts "Please input a number between 1 and 9"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move?
+  if valid_move?(board, index)
+    
   
 
 
